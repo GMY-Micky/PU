d5295176d6f0ee837b032762ca02a4b157bfa67b
GMY Micky <bcsf19a002@pucit.edu.pk>
2022-01-23 13:09:59 +0500

    add main page picture of department

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import "../css/header.css"
 
 const Header = () => {
-  return <div className='header'>
-      <ul className='left-list'>
-          <li className='left-list-item'><a href='#'>HOME</a></li>
-          <li className='left-list-item'><a href='#'>DERPARTMENTS</a></li>
-          <li className='left-list-item'><a href='#'>FACULTY</a></li>
-          <li className='left-list-item'><a href='#'>ACADEMICS</a></li>
-          <li className='left-list-item'><a href='#'>ADMISSION</a></li>
-      </ul>
+  return <div className='page-1'>
+      <div className='header'>
       <div className='header-logo'>
-        <img src="./assets/punjab-university-lahore-logo.png" alt="LOGO" />
-        <h3 className='uni-name'>Faculty of Computing & Information Technology</h3>
+        <div><img src="./assets/punjab-university-lahore-logo.png" alt="LOGO"/></div>
+        <div><span className='uni-name'>Faculty of Computing & Information Technology</span></div>
       </div>
-      <ul className='right-list'>
-          <li className='right-list-item'><a href='#'>STUDENT SOCIETIES</a></li>
-          <li className='right-list-item'><a href='#'>ALUMNUS</a></li>
-          <li className='right-list-item'><a href='#'>NOTICE BOARD</a></li>
-          <li className='right-list-item'><a href='#'>FCIT OFFICES</a></li>
-          <li className='right-list-item'><a href='#'>CONTACT US</a></li>
-      </ul>
+      <div><ul className='list'>
+          <li className='list-item'><a href='#'>HOME</a></li>
+          <li className='list-item'><a href='#'>DERPARTMENTS</a></li>
+          <li className='list-item'><a href='#'>FACULTY</a></li>
+          <li className='list-item'><a href='#'>ACADEMICS</a></li>
+          <li className='list-item'><a href='#'>ADMISSION</a></li>
+          <li className='list-item'><a href='#'>STUDENT SOCIETIES</a></li>
+          <li className='list-item'><a href='#'>ALUMNUS</a></li>
+          <li className='list-item'><a href='#'>NOTICE BOARD</a></li>
+          <li className='list-item'><a href='#'>FCIT OFFICES</a></li>
+          <li className='list-item'><a href='#'>CONTACT US</a></li>
+      </ul></div>
+  </div>
+  <div className='main-page-pic'><img src="./assets/main.jpg" alt="Department building" /></div>
   </div>;
 };
 
