c9261a19dbfc4be92fce9170090178a6310ca908
GMY Micky <bcsf19a002@pucit.edu.pk>
2022-01-23 15:17:26 +0500

    add smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
 import './App.css';
+import Departments from './components/Departments';
 import Header from './components/Header';
+import Scroll from './components/Scroll';
+
 
 function App() {
+ 
   return (
     <div className="App">
+      <Scroll/>
           <Header/>
+          <Departments/>
     </div>
   );
 }
---
+++ src/components/Departments.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Departments = () => {
+  return <div className='departments'>
+      <div className='dep-1'>
+        <img src="/assets/punjab-university-lahore-logo.jpg" alt="" />
+      </div>
+      <div className='dep-2'>
+      <img src="/assets/punjab-university-lahore-logo.jpg" alt="" />
+
+      </div>
+      <div className='dep-3'>
+      <img src="/assets/punjab-university-lahore-logo.jpg" alt="" />
+          
+      </div>
+      <div className='dep-4'>
+      <img src="/assets/punjab-university-lahore-logo.jpg" alt="" />
+
+      </div>
+  </div>;
+};
+
+export default Departments;
