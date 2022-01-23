31677436aee04dea3a20fd1571f06951dde2d183
GMY Micky <bcsf19a002@pucit.edu.pk>
2022-01-23 15:55:31 +0500

    add departments under header

--- src/components/Departments.jsx
+++ src/components/Departments.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import "../css/departments.css"
 
 const Departments = () => {
   return <div className='departments'>
       <div className='dep-1'>
-        <img src="/assets/punjab-university-lahore-logo.jpg" alt="" />
+        <img src="/assets/punjab-university-lahore-logo.png" alt="Punjab University Logo" /><br/>
+        <span className='department-name'>Department of Computer Science</span>
       </div>
       <div className='dep-2'>
-      <img src="/assets/punjab-university-lahore-logo.jpg" alt="" />
-
+      <img src="/assets/punjab-university-lahore-logo.png" alt="Punjab University Logo" /><br/>
+      <span className='department-name'>Department of Software Engineering</span>
       </div>
       <div className='dep-3'>
-      <img src="/assets/punjab-university-lahore-logo.jpg" alt="" />
-          
+      <img src="/assets/punjab-university-lahore-logo.png" alt="Punjab University Logo" /><br/>
+      <span className='department-name'>Department of Information Technology</span> 
       </div>
       <div className='dep-4'>
-      <img src="/assets/punjab-university-lahore-logo.jpg" alt="" />
-
+      <img src="/assets/punjab-university-lahore-logo.png" alt="Punjab University Logo" /><br/>
+      <span className='department-name'>Department of Data Science</span>
       </div>
   </div>;
 };
