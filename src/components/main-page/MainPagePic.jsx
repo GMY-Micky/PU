3385cb26bb835a29ba5955670a7b4ae3d0691f56
GMY Micky <bcsf19a002@pucit.edu.pk>
2022-01-24 23:37:25 +0500

    replace main pic with University video

--- src/components/main-page/MainPagePic.jsx
+++ src/components/main-page/MainPagePic.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import "./css/mainPagePic.css"
-import TypeWriterEffect from 'react-typewriter-effect';
+import React from "react";
+import "./css/mainPagePic.css";
+import TypeWriterEffect from "react-typewriter-effect";
 
 const MainPagePic = () => {
-  return <div className='main-page-pic'>
-  <div className="typewriter-text"><TypeWriterEffect
-            textStyle={{ color:"white",fontSize:'2.7vmax',fontWeight:'900' }}
-            startDelay={10}
-            cursorColor="white"
-            text="FACULTY OF COMPUTING & INFORMATION TECHNOLOGY"
-            typeSpeed={50}
-          /></div>
-  </div>;
+  return (
+    <div className="main-page-pic">
+       <div className="typewriter-text">
+        <TypeWriterEffect
+          textStyle={{ color: "rgba(47, 157, 198, 1)", fontSize: "3vmax", fontWeight: "900", textAlign:"center" }}
+          startDelay={10}
+          cursorColor="rgba(47, 157, 198, 1)"
+          text="FACULTY OF COMPUTING & INFORMATION TECHNOLOGY"
+          typeSpeed={50}
+        />
+      </div>
+      <div className="main-video">
+        <video src="./PU.mp4" width="100%" height="100%"
+          muted
+          autoPlay={"autoplay"}
+          preLoad="auto"
+          loop/>
+        </div>
+    </div>
+  );
 };
 
 export default MainPagePic;
--- src/components/main-page/MediaBox.jsx
+++ src/components/main-page/MediaBox.jsx
@@ -28,10 +28,10 @@ const MediaBox = () => {
 />
 </div>
 <div className="facebook-widget">
-<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFaculty.of.Computing.and.Information.Technology%2F&tabs=timeline&width=600&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="400" height="400" style={{border:"none",overflow:"hidden"}} scrolling="no" frameborder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> 
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FFaculty.of.Computing.and.Information.Technology%2F&tabs=timeline&width=600&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="400" height="400" style={{border:"none",overflow:"hidden"}} scrolling="no" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> 
 </div>
 <div className="youtube-video">
-<YouTube videoId="EkMbHkemjus" opts={opts} onReady={()=>videoOnReady()} />
+<YouTube videoId="EkMbHkemjus" opts={opts} onReady={(e)=>videoOnReady(e)} />
 </div>
     </div>;
 };
