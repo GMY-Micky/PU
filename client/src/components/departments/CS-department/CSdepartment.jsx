9f9964afbd02bdba52159afadd08adb45b6c49f6
GMY Micky <bcsf19a002@pucit.edu.pk>
2022-02-01 12:26:38 +0500

    boiler plates for department faculty info

---
+++ client/src/components/departments/CS-department/CSdepartment.jsx
@@ -0,0 +1,280 @@
+import React from "react";
+
+const CSdepartment = () => {
+  return (
+    <div className="cs-department">
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name">DR SHAHZAD SARWAR</span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item">
+              Dean Faculty of Computing and Information Technology
+            </li>
+            <li className="item">Chairman Department of Computer Science</li>
+            <li className="item">Professor </li>
+            <li className="item">
+              PhD Electrical Engineering and IT, Vienna University of Technology
+              Austria, 2008
+            </li>
+            <li className="item">MS Computer Science, LUMS Lahore, 2004</li>
+            <li className="item">B.Sc Civil Engineering, UET Taxila, 1998</li>
+            <li className="item">
+              Research / Teaching Interests: Internet of Things (IoT),
+              Machine-to-Machine (M2M) Communication, Data-centric Networking,
+              and Software Defined Networks (SDNs)
+            </li>
+            <li className="item">Research Lab: Modern Networking Lab (MNL) </li>
+            <li className="item">
+              Email: Principal@pucit.edu.pk, S.sarwar@pucit.edu.pk,
+              chairman.dcs@pucit.edu.pk, chairman.dcs@pu.edu.pk
+            </li>
+            <li className="item">Curriculum Vitae</li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+      <div className="person-1">
+        <div className="person-name-img">
+          <span className="person-name"></span>
+          <img src="" alt="" />
+        </div>
+        <div className="person-info">
+          <ul>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+            <li className="item"></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CSdepartment;
